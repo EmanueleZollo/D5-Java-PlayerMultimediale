d9fbe0c8fa7869f4d4fe7b841e71d53d8902cc20
Emanuele Zollo - NjS Studio <ez.studionjs@gmail.com>
2023-10-06 11:38:27 +0200

    first commit + abstact class

---
+++ src/Multimedia/MultimediaElement.java
@@ -0,0 +1,43 @@
+package Multimedia;
+
+public abstract class MultimediaElement {
+    protected String title;
+    protected int duration;
+
+
+  /*  CONSTRUCTOR PER IMMAGINE*/
+    public MultimediaElement(String title) {
+        this.title = title;
+    }
+
+    /*CONSTRUCTOR PER VIDEO E REGISTRAZIONE AUDIO*/
+    public MultimediaElement(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    /*  METODI   */
+    public void play(){
+        for (int i = 0; i < this.duration; i++) {
+            System.out.println(this.title);
+        }
+    }
+
+
+    /*   SETTERS & GETTERS  */
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
